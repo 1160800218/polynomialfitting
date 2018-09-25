1bf1379f11b6df3acf41ed5c1ddf0673d6797f0b
1160800218 <lzjjeffery@gmail.com>
2018-09-25 15:28:19 +0800

    将fitting()和get_best_regular()分离；重命名了部分函数；将有无正则项的解析法分离。

--- polynomialfitting.py
+++ polynomialfitting.py
@@ -3,8 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+
 # 梯度下降法
-def gradientDescent(A, x, b, alpha):
+def gradient_descent(A, x, b, alpha):
     epsilon = 1e-3
     grad = np.dot(A, x) - b                             # 梯度grad(k)
     k = 1
@@ -15,8 +16,9 @@ def gradientDescent(A, x, b, alpha):
     print '梯度下降法的迭代次数为：', k, '次'
     return x
 
+
 # 共轭梯度法
-def conjugateGradient(A, x, b):
+def conjugate_gradient(A, x, b):
     epsilon = 1e-4
     r = b - np.dot(A, x)                                # 残差r(0)=b=A*x(0)
     p = r                                               # p(0)=r(0)
@@ -33,6 +35,7 @@ def conjugateGradient(A, x, b):
     print '共轭梯度法的迭代次数为：', len(b), '次'
     return x
 
+
 # 多项式拟合
 class PolynomialFitting(object):
     def __init__(self, degree, scale):
@@ -42,7 +45,7 @@ class PolynomialFitting(object):
         self.valid_scale = int((scale - self.train_scale) / 2)
         self.test_scale = int(scale - self.train_scale - self.valid_scale)
     # 生成样本集合
-    def generatingX(self):
+    def generating_X(self):
         self.x = np.arange(-1.0, 1.0, 2.0/self.scale)
         self.train_x = np.ones(self.train_scale)      # 训练集合
         self.valid_x = np.ones(self.valid_scale)      # 验证集合
@@ -71,7 +74,7 @@ class PolynomialFitting(object):
                     self.test_X[i, j] = math.pow(self.test_x[i], j)
 
     # 生成含0均值的高斯噪声的目标值t集合
-    def generatingT(self):
+    def generating_T(self):
         noise = np.random.normal(loc=0.0, scale=0.01, size=len(self.x))                 # 0均值，0.01标准差的噪声
         self.T = (np.array(np.sin(self.x * np.pi * 2)) + noise).reshape(self.scale, 1)
         self.train_T = np.ones(self.train_scale).reshape(self.train_scale, 1)           # 训练目标值
@@ -90,93 +93,121 @@ class PolynomialFitting(object):
             self.test_T[self.test_scale - 1] = self.T[2 * k - 1]
 
     # 解析解求系数
-    def calculateW(self, regular):
+    def calculate_W(self):
         self.W = np.linalg.inv(np.dot(self.train_X.T, self.train_X)).dot(self.train_X.T).dot(self.train_T)
+    def calculate_W_with_regular(self, regular):
         self.W_regular = 2 * np.linalg.inv(2 * np.dot(self.train_X.T, self.train_X) + regular * np.eye(self.train_X.shape[1])).dot(self.train_X.T).dot(self.train_T)
-        # print self.W
-        # print self.W_regular
 
     # 梯度下降、共轭梯度法求系数
-    def calculateWWithGrad(self, regular, method):
+    def calculate_W_with_grad(self, regular, method):
         w = np.ones(self.degree + 1)
         w = w.reshape(self.degree + 1, 1)
         A = (2 * np.dot(self.train_X.T, self.train_X) + regular * np.eye(self.train_X.shape[1])) / self.train_scale         # 正定矩阵A
         b = 2 * np.dot(self.train_X.T, self.train_T) / self.train_scale                                                     # 目标矩阵b
         if method == '梯度下降法':
-            self.W_gd = gradientDescent(A, w, b, 0.1)
+            self.W_gd = gradient_descent(A, w, b, 0.1)
         if method == '共轭梯度法':
-            self.W_cg = conjugateGradient(A, w, b)
+            self.W_cg = conjugate_gradient(A, w, b)
 
     # 求损失函数
-    def loss(self):  # 参数pf为PolynomialFitting类对象
-        loss = (np.dot(self.valid_X, self.W) - self.valid_T).T.dot(np.dot(self.valid_X, self.W) - self.valid_T)[
+    def loss(self):
+        loss = ((np.dot(self.valid_X, self.W) - self.valid_T).T.dot(np.dot(self.valid_X, self.W) - self.valid_T))[
                    0, 0] / self.valid_scale
         return loss
-    def lossWithRegular(self, regular, W):
+
+    def loss_with_regular(self, regular, W):
         loss = ((np.dot(self.valid_X, W) - self.valid_T).T.dot(
             np.dot(self.valid_X, W) - self.valid_T)
                 + regular * np.linalg.norm(W, ord=2) ** 2 / 2)[0, 0] / self.valid_scale
         return loss
 
     # 获得最优超参数（惩罚系数）
-    def getBestRegular(self, rs, method):
+    def get_best_regular(self, rs, method):
+        print method
         loss = 65535
         minloss = 65535
         self.bestr = 0
         W = np.ones(self.degree + 1)
         self.bestW = np.ones(self.degree + 1)               # 惩罚项取最优时的系数向量
         for r in rs:
-            if method == '解析法':
-                PolynomialFitting.calculateW(self, r)
-                loss = PolynomialFitting.lossWithRegular(self, r, self.W_regular)
+            if method == '带正则项解析法':
+                PolynomialFitting.calculate_W_with_regular(self, r)
+                loss = PolynomialFitting.loss_with_regular(self, r, self.W_regular)
                 W = self.W_regular
             if method == '梯度下降法':
-                PolynomialFitting.calculateWWithGrad(self, r, method)
-                loss = PolynomialFitting.lossWithRegular(self, r, self.W_gd)
+                PolynomialFitting.calculate_W_with_grad(self, r, method)
+                loss = PolynomialFitting.loss_with_regular(self, r, self.W_gd)
                 W = self.W_gd
             if method == '共轭梯度法':
-                PolynomialFitting.calculateWWithGrad(self, r, method)
-                loss = PolynomialFitting.lossWithRegular(self, r, self.W_cg)
+                PolynomialFitting.calculate_W_with_grad(self, r, method)
+                loss = PolynomialFitting.loss_with_regular(self, r, self.W_cg)
                 W = self.W_cg
             if loss < minloss:
                 minloss = loss
                 self.bestr = r
                 self.bestW = W
-            print loss
+            print "惩罚系数数为", r, "时loss =", loss
         print '惩罚系数取', self.bestr, '时代价最低，最优代价为：', minloss
 
-    def fitting(self, regulars, method):
-        # x = np.arange(-1.0, 1.0, 0.001)
-        # y = np.sin(2 * np.pi * x)
-        # ax[0][0].plot(x, y)
-        PolynomialFitting.getBestRegular(self, regulars, method)
-        # if method == '解析法':
-        #     ax[0][0].plot(self.train_x, self.train_X.dot(self.W), 'b.-')
-        #     ax[0][1].plot(self.train_x, self.train_X.dot(self.bestW), 'r+-')
-        # if method == '梯度下降法':
-        #     ax[1][0].plot(self.train_x, self.train_X.dot(self.bestW), 'g*-')
-        # if method == '共轭梯度法':
-        #     ax[1][1].plot(self.train_x, self.train_X.dot(self.bestW), 'yo-')
-        # plt.plot(self.train_x, self.train_T, 'g.')
-
+    def fitting(self, regular, method):
+        W = np.ones(self.degree + 1)
+        loss = -1
+        if method == '解析法':
+            PolynomialFitting.calculate_W(self)
+            loss = PolynomialFitting.loss(self)
+        if method == '带正则项解析法':
+            PolynomialFitting.calculate_W_with_regular(self, regular)
+            loss = PolynomialFitting.loss_with_regular(self, regular, self.W_regular)
+        if method == '梯度下降法':
+            PolynomialFitting.calculate_W_with_grad(self, regular, method)
+            loss = PolynomialFitting.loss_with_regular(self, regular, self.W_gd)
+        if method == '共轭梯度法':
+            PolynomialFitting.calculate_W_with_grad(self, regular, method)
+            loss = PolynomialFitting.loss_with_regular(self, regular, self.W_cg)
+        print method, "loss =", loss
 
 
 def main():
-    p = PolynomialFitting(degree=9, scale=30)
-    p.generatingX()
-    p.generatingT()
-    regulars = [0.00001, 0.0001, 0.001]
-    methods = ['解析法', '梯度下降法', '共轭梯度法']
+    p = PolynomialFitting(degree=50, scale=60)
+    p.generating_X()
+    p.generating_T()
+    # 确定惩罚系数比较四种方法的拟合效果
+    methods = ['解析法', '带正则项解析法', '梯度下降法', '共轭梯度法']
     for method in methods:
-        p.fitting(regulars, method)
+        p.fitting(0.00001, method)
+    print "\n"
 
+    # 训练拟合图
     fig1 = plt.figure()
     fig1.suptitle("degree=" + str(9))
     ax = fig1.subplots(2, 2)
+    for i in range(2):
+        for j in range(2):
+            ax[i][j].plot(p.train_x, p.train_T, 'g.')
     ax[0][0].plot(p.train_x, p.train_X.dot(p.W), 'b.-')
-    ax[0][1].plot(p.train_x, p.train_X.dot(p.bestW), 'r+-')
-    ax[1][0].plot(p.train_x, p.train_X.dot(p.bestW), 'g*-')
-    ax[1][1].plot(p.train_x, p.train_X.dot(p.bestW), 'yo-')
+    ax[0][1].plot(p.train_x, p.train_X.dot(p.W_regular), 'r+-')
+    ax[1][0].plot(p.train_x, p.train_X.dot(p.W_gd), 'g*-')
+    ax[1][1].plot(p.train_x, p.train_X.dot(p.W_cg), 'yo-')
+
+    # 验证尼拟合图
+    fig2 = plt.figure()
+    fig2.suptitle("degree=" + str(9))
+    ax2 = fig2.subplots(2, 2)
+    for i in range(2):
+        for j in range(2):
+            ax2[i][j].plot(p.valid_x, p.valid_T, 'g.')
+    ax2[0][0].plot(p.valid_x, p.valid_X.dot(p.W), 'b.-')
+    ax2[0][1].plot(p.valid_x, p.valid_X.dot(p.W_regular), 'r+-')
+    ax2[1][0].plot(p.valid_x, p.valid_X.dot(p.W_gd), 'g*-')
+    ax2[1][1].plot(p.valid_x, p.valid_X.dot(p.W_cg), 'yo-')
+
+    # 给定不同惩罚系数筛选最优
+    methods = ['带正则项解析法', '梯度下降法', '共轭梯度法']
+    regulars = [10**-15, 10**-10, 10**-5, 10**-4, 10**-3, 10**-2, 0.1, 1]
+    print "regulars =", regulars
+    for method in methods:
+        p.get_best_regular(regulars, method)
+        print ""                # 空一行
 
     plt.show()
 
